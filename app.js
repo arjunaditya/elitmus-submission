20a576e273b7f225ddeada72da17ba163a7266d5
Ayush Ranjan <ayushranjan6456@gmail.com>
2022-04-11 23:53:50 +0530

    colors added

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const containerBox = document.querySelector(".guess_container");
 let guessBox = document.createElement("div");
-let level = 5;
+let level = 4;
 
-guessBox.classList.add("guess_box");
-containerBox.appendChild(guessBox);
+let diff = Math.floor(Math.random()*level)
+
+//Set colour for 4 blocks
+var red = Math.floor(Math.random()*255);
+var green = Math.floor(Math.random()*255);
+var blue = Math.floor(Math.random()*255);
+
+//Set colour for diff blocks
+var red_diff = Math.floor(Math.random()*255);
+var green_diff = Math.floor(Math.random()*255);
+var blue_diff = Math.floor(Math.random()*255);
 
 while (level-- > 0) {
-  containerBox.innerHTML += `<div class="guess_box"></div>`;
+  let guessBox = document.createElement("div");
+  guessBox.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+  guessBox.classList.add("guess_box");
+  containerBox.appendChild(guessBox);
+  if(level == diff){
+    guessBox.style.backgroundColor = `rgb(${red_diff}, ${green_diff}, ${blue_diff})`;
+    guessBox.classList.add("guess_box");
+    containerBox.appendChild(guessBox);
+  }
 }
+
+guessBox.addEventListener()
