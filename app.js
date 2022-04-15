260b6f3ec48390eec15b65d3e920fd648c9f5800
cryptus-neoxys <sharma.dev4242@gmail.com>
2022-04-16 00:54:21 +0530

    structure game logic

--- app.js
+++ app.js
@@ -1,29 +1,66 @@
 const containerBox = document.querySelector(".guess_container");
-let guessBox = document.createElement("div");
-let level = 4;
 
-let diff = Math.floor(Math.random()*level)
+// start game
+const startGame = document.querySelector(".start-game");
+startGame.addEventListener("click", game);
 
-//Set colour for 4 blocks
-var red = Math.floor(Math.random()*255);
-var green = Math.floor(Math.random()*255);
-var blue = Math.floor(Math.random()*255);
-
-//Set colour for diff blocks
-var red_diff = Math.floor(Math.random()*255);
-var green_diff = Math.floor(Math.random()*255);
-var blue_diff = Math.floor(Math.random()*255);
-
-while (level-- > 0) {
-  let guessBox = document.createElement("div");
-  guessBox.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-  guessBox.classList.add("guess_box");
-  containerBox.appendChild(guessBox);
-  if(level == diff){
-    guessBox.style.backgroundColor = `rgb(${red_diff}, ${green_diff}, ${blue_diff})`;
-    guessBox.classList.add("guess_box");
-    containerBox.appendChild(guessBox);
-  }
+function game() {
+  startGame.classList.add("hide");
+  setGame();
 }
 
-guessBox.addEventListener()
+// level info
+
+// set game
+let score = 0;
+let roundEnded = true;
+let guessBox = document.createElement("div");
+let level = 4;
+let diff = undefined;
+
+function setGame() {
+  diff = Math.floor(Math.random() * level);
+
+  //Set colour for 4 blocks
+  let red = Math.floor(Math.random() * 255);
+  let green = Math.floor(Math.random() * 255);
+  let blue = Math.floor(Math.random() * 255);
+
+  //Set colour for diff blocks
+  let red_diff = Math.floor(Math.random() * 255);
+  let green_diff = Math.floor(Math.random() * 255);
+  let blue_diff = Math.floor(Math.random() * 255);
+
+  while (level-- > 0) {
+    roundEnded = false;
+    let guessBox = document.createElement("div");
+    guessBox.addEventListener("click", () => checkTile(guessBox.id));
+    guessBox.id = `guess-box-${level}`;
+    guessBox.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    guessBox.classList.add("guess_box");
+    containerBox.appendChild(guessBox);
+    if (level == diff) {
+      guessBox.style.backgroundColor = `rgb(${red_diff}, ${green_diff}, ${blue_diff})`;
+      guessBox.classList.add("guess_box");
+      containerBox.appendChild(guessBox);
+    }
+  }
+  return diff;
+}
+
+// select tile
+// check for correct tile
+function checkTile(boxID) {
+  if (!roundEnded && boxID.split("guess-box-")[1] == diff) {
+    score++;
+  }
+  console.log(score);
+  roundEnded = true;
+}
+// reset game
+
+// update game
+
+// next level
+
+// guessBox.addEventListener();
