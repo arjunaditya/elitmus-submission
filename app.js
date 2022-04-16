893ebdc19148e2be0cea0f7e0fe3f357130ee3cb
cryptus-neoxys <sharma.dev4242@gmail.com>
2022-04-16 15:48:08 +0530

    feat: game loop, basic logic

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+/**
+ * Returns a number whose value is limited to the given range.
+ *
+ * Example: limit the output of this computation to between 0 and 255
+ * (x * 255).clamp(0, 255)
+ *
+ * @param {Number} min The lower boundary of the output range
+ * @param {Number} max The upper boundary of the output range
+ * @returns A number in the range [min, max]
+ * @type Number
+ */
+Number.prototype.clamp = function (min, max) {
+  return Math.min(Math.max(this, min), max);
+};
+
 const containerBox = document.querySelector(".guess_container");
 
 // start game
@@ -16,9 +31,12 @@ let score = 0;
 let roundEnded = true;
 let guessBox = document.createElement("div");
 let level = 4;
+let colorRange = 255;
 let diff = undefined;
 
 function setGame() {
+  roundEnded = false;
+  containerBox.innerHTML = "";
   diff = Math.floor(Math.random() * level);
 
   //Set colour for 4 blocks
@@ -27,19 +45,25 @@ function setGame() {
   let blue = Math.floor(Math.random() * 255);
 
   //Set colour for diff blocks
-  let red_diff = Math.floor(Math.random() * 255);
-  let green_diff = Math.floor(Math.random() * 255);
-  let blue_diff = Math.floor(Math.random() * 255);
+  // (color plus or minus a random number between the range) clamped to valid RGB range
+  let red_diff =
+    red +
+    (Math.random() < 0.5 ? -1 : 1 * Math.floor(Math.random() * colorRange));
+  let green_diff =
+    green +
+    (Math.random() < 0.5 ? -1 : 1 * Math.floor(Math.random() * colorRange));
+  let blue_diff =
+    blue +
+    (Math.random() < 0.5 ? -1 : 1 * Math.floor(Math.random() * colorRange));
 
-  while (level-- > 0) {
-    roundEnded = false;
+  for (let i = 0; i < level; ++i) {
     let guessBox = document.createElement("div");
     guessBox.addEventListener("click", () => checkTile(guessBox.id));
-    guessBox.id = `guess-box-${level}`;
+    guessBox.id = `guess-box-${i}`;
     guessBox.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
     guessBox.classList.add("guess_box");
     containerBox.appendChild(guessBox);
-    if (level == diff) {
+    if (i == diff) {
       guessBox.style.backgroundColor = `rgb(${red_diff}, ${green_diff}, ${blue_diff})`;
       guessBox.classList.add("guess_box");
       containerBox.appendChild(guessBox);
@@ -53,9 +77,29 @@ function setGame() {
 function checkTile(boxID) {
   if (!roundEnded && boxID.split("guess-box-")[1] == diff) {
     score++;
+  } else {
+    score--;
   }
   console.log(score);
   roundEnded = true;
+  if (
+    (level < 8 && score > 4) ||
+    (level < 11 && score > 3) ||
+    (level < 13 && score > 2) ||
+    level >= 13
+  ) {
+    level = Math.min(level + 1, 15);
+  }
+  if (
+    (level < 8 && score > 6) ||
+    (level < 11 && score > 5) ||
+    (level < 13 && score > 3) ||
+    level >= 13
+  ) {
+    colorRange = Math.max(10, colorRange - 15);
+    if (level < 13) score = 0;
+  }
+  setGame();
 }
 // reset game
 
